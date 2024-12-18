f025190679d6d2bcd7c1f3adbf21d0bca90cfe3e
Muhammad Hasnain <hasnain0893@gmail.com>
2024-12-18 15:11:17 +0500

    footer set

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import './Footer.css'
 // import { FaFacebook, FaXTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
 
 function Footer() {
@@ -14,45 +15,46 @@ function Footer() {
         >
             <Container>
                 <Row>
-                    <Col md={3}>
-                        <h6>Recommended</h6>
-                        <ul style={{ listStyleType: "none", padding: 0, lineHeight: "2" }}>
-                            <li>realme C75</li>
-                            <li>realme 13+ 5G</li>
-                            <li>realme Note 60</li>
-                            <li>realme C61</li>
-                            <li>realme C65</li>
-                            <li>realme 12</li>
-                            <li>realme 12+ 5G</li>
-                        </ul>
-                    </Col>
-
-                    <Col md={3}>
-                        <h6>Support</h6>
-                        <ul style={{ listStyleType: "none", padding: 0, lineHeight: "2" }}>
-                            <li>FAQ</li>
-                            <li>Troubleshooting</li>
-                        </ul>
-                    </Col>
-
-                    <Col md={3}>
-                        <h6>About realme</h6>
-                        <ul style={{ listStyleType: "none", padding: 0, lineHeight: "2" }}>
-                            <li>Our Brand</li>
-                            <li>Newsroom</li>
-                        </ul>
-                    </Col>
-
-                    <Col md={3}>
-                        <h6>Contact realme</h6>
-                        <ul style={{ listStyleType: "none", padding: 0, lineHeight: "2" }}>
-                            <li>Whatsapp</li>
-                            <li>service.pk@realme.com</li>
-                        </ul>
-                        <h4 style={{ marginTop: "10px" }}>04238048018</h4>
-                        <p style={{ marginBottom: "5px" }}>09:30 - 18:00, MON - SAT</p>
-                        <p>Exclude Holidays</p>
-                        {/* <div>
+                    <div className="footerMainDiv">
+                        <div className="div1">
+                            <div>
+                                <h6>Recommended</h6>
+                                <ul style={{ listStyleType: "none", padding: 0, lineHeight: "2" }}>
+                                    <li>realme C75</li>
+                                    <li>realme 13+ 5G</li>
+                                    <li>realme Note 60</li>
+                                    <li>realme C61</li>
+                                    <li>realme C65</li>
+                                    <li>realme 12</li>
+                                    <li>realme 12+ 5G</li>
+                                </ul>
+                            </div>
+                            <div>
+                                <h6>Support</h6>
+                                <ul style={{ listStyleType: "none", padding: 0, lineHeight: "2" }}>
+                                    <li>FAQ</li>
+                                    <li>Troubleshooting</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="div1">
+                            <div>
+                                <h6>About realme</h6>
+                                <ul style={{ listStyleType: "none", padding: 0, lineHeight: "2" }}>
+                                    <li>Our Brand</li>
+                                    <li>Newsroom</li>
+                                </ul>
+                            </div>
+                            <div>
+                                <h6>Contact realme</h6>
+                                <ul style={{ listStyleType: "none", padding: 0, lineHeight: "2" }}>
+                                    <li>Whatsapp</li>
+                                    <li>service.pk@realme.com</li>
+                                </ul>
+                                <h4 style={{ marginTop: "10px" }}>04238048018</h4>
+                                <p style={{ marginBottom: "5px" }}>09:30 - 18:00, MON - SAT</p>
+                                <p>Exclude Holidays</p>
+                                {/* <div>
                             <FaFacebook
                                 style={{ marginRight: "15px", fontSize: "20px", cursor: "pointer" }}
                             />
@@ -66,7 +68,9 @@ function Footer() {
                                 style={{ fontSize: "20px", cursor: "pointer" }}
                             />
                         </div> */}
-                    </Col>
+                            </div>
+                        </div>
+                    </div>
                 </Row>
                 <hr style={{ backgroundColor: "#444", height: "1px" }} />
                 <Row className="text-center">
